34c54a19df3e8f06597a0e82282b3810556fc2e5
Faqihul Muqoddam <92261904+damskuyyy@users.noreply.github.com>
2024-08-21 23:45:34 +0700

    push dams

---
+++ src/pages/ProductsDams.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProductsDams = () => {
+    return ( <div>
+        <h1 className="text-3xl font-bold">Data Products Dams</h1>
+    </div> );
+}
+ 
+export default ProductsDams;
