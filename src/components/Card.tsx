f6eae92df00bf7cf5a96a12cd35e1f752e2ba161
Faqihul Muqoddam <92261904+damskuyyy@users.noreply.github.com>
2024-08-22 09:23:31 +0700

    done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,15 @@
-// src/components/Card.tsx
 import React from 'react';
 
-interface CardProps {
+type CardProps = {
   title: string;
   description: string;
-  imageUrl: string;
+  images: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
+const Card: React.FC<CardProps> = ({ title, description, images }) => {
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
-      <img className="w-full h-48 object-cover" src={imageUrl} alt={title} />
+      <img className="w-full h-48 object-cover" src={images} alt={title} />
       <div className="p-4">
         <h3 className="text-lg font-semibold mb-2">{title}</h3>
         <p className="text-gray-600 text-sm">{description}</p>
--- src/pages/ProductsDams.tsx
+++ src/pages/ProductsDams.tsx
@@ -1,36 +1,35 @@
 // src/pages/Home.tsx
 import React from 'react';
 import Card from '../components/Card';
+import { useEffect, useState } from 'react';
+
+type Product = {
+  title: string;
+  description: string;
+  images: string;
+}
 
 const ProductsDams: React.FC = () => {
-  const cards = [
-    {
-      title: 'Card Title 1',
-      description: 'This is a brief description of the card content.',
-      imageUrl: 'https://via.placeholder.com/300',
-    },
-    {
-      title: 'Card Title 2',
-      description: 'This is a brief description of the card content.',
-      imageUrl: 'https://via.placeholder.com/300',
-    },
-    {
-      title: 'Card Title 3',
-      description: 'This is a brief description of the card content.',
-      imageUrl: 'https://via.placeholder.com/300',
-    },
-  ];
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    // Fetch data dari DummyJSON
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl text-center text-gray-800 font-bold mb-6">Data Product Dams</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {cards.map((card, index) => (
+        {products.map((item, index) => (
           <Card
             key={index}
-            title={card.title}
-            description={card.description}
-            imageUrl={card.imageUrl}
+            title={item.title}
+            description={item.description}
+            images={item.images}
           />
         ))}
       </div>
