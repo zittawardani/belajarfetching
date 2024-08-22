5c8ed531b7531a6268f08f1c851f092cecb01062
Nafiskurniatul <92704434+Nafiskurniatul@users.noreply.github.com>
2024-08-22 09:58:42 +0700

    productnafis

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ProductZitta from "./pages/ProductZitta";
+import ProductNafis from "./pages/ProductNafis";
 
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/", element:<ProductZitta/>
+      path: "/", children:[{path: "productzitta", element: <ProductZitta/>}]
     },
+    {
+      path: "/", children:[{path: "productNafis", element: <ProductNafis/>}]
+    }
   ]);
   return <RouterProvider router={router}></RouterProvider>;
 }
 
-export default App;
+export default App;
---
+++ src/pages/ProductNafis.tsx
@@ -0,0 +1,114 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode: string;
+  };
+  thumbnail: string;
+  images: string[];
+}
+
+interface Review {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+const ProductNafis: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getProductsData = async () => {
+    try {
+      const resp = await axios.get("https://dummyjson.com/products");
+      setProducts(resp.data.products); // Assigning the array of products
+      console.log("data product: ", resp.data.products);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getProductsData();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {products.map((item) => (
+        <div
+          key={item.id}
+          className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
+          <a href="#">
+            <img
+              className="rounded-t-lg"
+              src={item.thumbnail}
+              alt={item.title}
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {item.title}
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              {item.description}
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductNafis;
